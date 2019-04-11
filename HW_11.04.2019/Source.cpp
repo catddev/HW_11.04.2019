39e8b98dd5d40e4162daeeb18b9ae1f80c7d904b
айкеновад <@205_8>
2019-04-11 13:26:14 +0600

    -

--- HW_11.04.2019/Source.cpp
+++ HW_11.04.2019/Source.cpp
@@ -1,1 +1,29 @@
-﻿
+﻿#include "Reservoir.h"
+
+int main() {
+	
+	Reservoir r1(7, 8);
+	Reservoir r2(r1);
+	Reservoir r3(10, 10);
+	
+	r1.print();
+	r2.print();
+	r3.print();
+	
+
+	if (r1 == r2) cout << "r1 and r2 are equal" << endl;
+	else cout << "r1 and r2 are not equal" << endl;
+	if (r1 == r3) cout << "r1 and r3 equal" << endl;
+	else cout << "r1 and r3 are not equal" << endl;
+
+	if (r2 < r3) cout << "r2 is smaller" << endl;
+	if (r3 > r1) cout << "r3 is greater" << endl;
+
+	r1 += 100;
+	r2 -= 4;
+
+	r1.print();
+	r2.print();
+
+	system("pause");
+}
