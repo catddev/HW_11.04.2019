b8a0c6909d53c0e08e31cb17bc3e2c177bac8c60
айкеновад <@205_8>
2019-04-11 13:18:42 +0600

    Добавьте файлы проекта.

---
+++ HW_11.04.2019/Reservoir.cpp
@@ -0,0 +1,76 @@
+﻿#include "Reservoir.h"
+
+Reservoir::Reservoir(double a)
+{
+	this->a = this->b = a;
+}
+
+Reservoir::Reservoir(double a, double b) {
+	this->a = a;
+	this->b = b;
+}
+
+double Reservoir::area()
+{
+	return a * b;
+}
+
+void Reservoir::setA(double a)
+{
+	this->a = a;
+}
+
+void Reservoir::setB(double b)
+{
+	this->b = b;
+}
+
+double Reservoir::getA()
+{
+	return a;
+}
+
+double Reservoir::getB()
+{
+	return b;
+}
+
+void Reservoir::print()
+{
+	cout << a << " " << b << endl;
+}
+
+bool Reservoir::operator==(Reservoir obj)
+{
+	return (area() == obj.area());
+}
+
+bool Reservoir::operator>(Reservoir obj)
+{
+	if (area() > obj.area())
+		return true;
+	else
+		return false;
+}
+
+bool Reservoir::operator<(Reservoir obj)
+{
+	if (area() < obj.area())
+		return true;
+	else
+		return false;
+}
+
+Reservoir Reservoir::operator+=(double n)
+{
+	this->a += n;
+	this->b += n;
+	return *this;
+}
+
+Reservoir Reservoir::operator-=(double n)
+{
+	this->a -= n;
+	this->b -= n;
+	return *this;
+}
---
+++ HW_11.04.2019/Reservoir.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+//класс Reservoir(водоем)
+//	Проверка на равенство площади водной поверхности(операция == )
+//	Увеличение и уменьшение объема резервуара на переделенное число(операции +=, -=)
+//	Сравнение двух резервуаров по площади водной поверхности(операция > )
+
+class Reservoir {
+	double a;
+	double b;
+public:
+	Reservoir() :a(0), b(0) {}
+	Reservoir(double a);
+	Reservoir(double a, double b);
+	double area();
+
+	void setA(double a);
+	void setB(double b);
+	double getA();
+	double getB();
+	void print();
+
+	bool operator==(Reservoir obj);
+	bool operator>(Reservoir obj);
+	bool operator<(Reservoir obj);
+	Reservoir operator+=(double n);
+	Reservoir operator-=(double n);
+};
